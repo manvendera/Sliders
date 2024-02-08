d15fc735f9371367eb47208c27497f2d7f3d9853
manvendera pathak <manvenderapathak165@gmail.com>
2024-02-08 13:11:03 +0530

    slider function is completed

---
+++ src/components/Testimonials.js
@@ -0,0 +1,47 @@
+// import React, { useState } from 'react'
+import Card from './Card'
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi"
+// import { useState } from 'react';
+import { useState } from 'react'
+export default function Testimonials({reviews}) {
+    let[index,setIndex] =  useState(0)
+    function leftShiftHandler() {
+      
+      if (index -1 <= 0) {
+        setIndex(reviews.length -1)
+      } else {
+        setIndex(index-1)
+      }
+    }
+    function rightShiftHandler() {
+     
+      if (index +1 >= reviews.length ) {
+        setIndex(0)
+      } else {
+        
+        setIndex(index +1 )
+      }
+    }
+ function surpriceShiftHandler() {
+  let randomIndex = Math.floor(Math.random()* reviews.length)
+  setIndex(randomIndex)
+ }
+  return (
+    <div className='flex flex-col w-[85vw] md:w-[700px] bg-white justify-center items-center mt-10 p-10 transition-all duration-700 hover:shadow-xl rounded-md'>
+        <Card review={reviews[index]}></Card>
+      
+            <div className='flex text-3xl mt-5 gap-3 text-violet-400 font-bold justify-center'>
+                <button onClick={leftShiftHandler} className='cursor-pointer hover:text-voilet-500'>
+                    <FiChevronLeft></FiChevronLeft>
+                </button>
+                <button onClick={rightShiftHandler} className='cursor-pointer hover:text-violet-500'>
+                    <FiChevronRight></FiChevronRight>
+                </button>
+            </div>
+            <div>
+                <button onClick={surpriceShiftHandler}className='bg-violet-400 hover:bg-violet-500 transition-all duration-200 px-10 py-2 rounded-md font-bold text-white text-lg'>Surprise Me</button>
+            </div>
+    </div>
+  )
+}
+       
---
+++ src/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+    {
+      id: 1,
+      name: "Watermelon ",
+      job: "Source of Hydration",
+      image: "https://images.pexels.com/photos/4860479/pexels-photo-4860479.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      text: "Watermelon, with its refreshing sweetness and vibrant colors, holds a special place in the summertime palate. This beloved fruit, scientifically known as Citrullus lanatus, is not just a delicious treat but also a source of hydration and essential nutrients.",
+    },
+    {
+      id: 2,
+      name: "Orange Juice",
+      job: "Source of Vitamin D",
+      image: "https://images.pexels.com/photos/338713/pexels-photo-338713.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      text: "Orange juice is a beloved beverage enjoyed by millions worldwide for its refreshing taste and nutritional benefits. Made from the juice extracted from oranges, this vibrant drink is packed with vitamin C, potassium, and other essential nutrients, making it a popular choice for breakfast or as a refreshing pick-me-up throughout the day",
+    },
+    {
+      id: 3,
+      name: " Coconuts",
+      job: "Slightly sweeter",
+      image: "https://images.pexels.com/photos/1803516/pexels-photo-1803516.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      text: "Coconuts are nature's versatile wonder, known for their distinct shape, tropical origin, and multifaceted uses. These large, brown fruits come from the coconut palm tree (Cocos nucifera), which thrives in warm, coastal climates. Here's a closer look at the fascinating world of coconuts:",
+    },
+    {
+      id: 4,
+      name: "Lemon",
+      job: " Incredibly refreshing",
+      image: "https://images.pexels.com/photos/161573/background-water-breakfast-bright-161573.jpeg",
+      text: "Lemons are like a burst of sunshine in the world of fruits. These bright, yellow citrus fruits are not only incredibly refreshing but also versatile in their use, adding a zingy flavor to both sweet and savory dishes",
+    },
+    {
+      id: 5,
+      name: "Mango",
+      job: "Dietary fiber",
+      image: "https://images.pexels.com/photos/1420042/pexels-photo-1420042.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      text: "Mangoes are not only delicious but also incredibly nutritious. They are rich in vitamins A, C, and E, as well as dietary fiber and antioxidants. Consuming mangoes regularly may contribute to improved immunity, skin health, and digestion.",
+    },
+  ];
+  
+  export default reviews;
+  
